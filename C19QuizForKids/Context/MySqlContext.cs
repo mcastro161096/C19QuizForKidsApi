ffd6b5a15ed811ecdbe8a38fe9446987f5ef80e3
dsfgdfg <dfgdfg@gmail.com>
2021-11-15 16:01:07 -0300

    Configurando DbContext.

---
+++ C19QuizForKids/Context/MySqlContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace C19QuizForKids.Context
+{
+    public class MySqlContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public MySqlContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to mysql with connection string from app settings
+            var connectionString = Configuration.GetConnectionString("WebApiDatabase");
+            options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        }
+
+        //public DbSet<User> Users { get; set; }
+    }
+}
