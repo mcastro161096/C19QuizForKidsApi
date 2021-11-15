957d7cc1b45365c1c588d8c56078dd1e3e3156df
dsfgdfg <dfgdfg@gmail.com>
2021-11-15 16:24:06 -0300

    Add entidades.

---
+++ C19QuizForKids/Entities/Card.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace C19QuizForKids.Entities
+{
+    public class Card
+    {
+        public int Id { get; set; }
+        public string Pergunta { get; set; }
+        public string Explicacao { get; set; }
+        public string Imagem { get; set; }
+        public bool Resposta { get; set; }
+    }
+}
---
+++ C19QuizForKids/Entities/Ranking.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace C19QuizForKids.Entities
+{
+    public class Ranking
+    {
+        public int Id { get; set; }
+        public string NickName { get; set; }
+        public int Pontuacao { get; set; }
+    }
+}
