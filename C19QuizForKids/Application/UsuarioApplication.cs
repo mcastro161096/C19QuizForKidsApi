59d6c118939855a921e503c562b9c1b723e69db3
dsfgdfg <MATHEUSCASTRO161096@gmail.com>
2021-11-21 10:31:44 -0300

    Ajustes no endpoint Salvar Usuario.

--- C19QuizForKids/Application/UsuarioApplication.cs
+++ C19QuizForKids/Application/UsuarioApplication.cs
@@ -1,5 +1,6 @@
 ﻿using C19QuizForKids.Context;
 using C19QuizForKids.Entities;
+using C19QuizForKids.Repository;
 using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json.Linq;
 using System;
@@ -21,21 +22,23 @@ namespace C19QuizForKids.Application
         public JObject Salvar(Usuario usuario)
         {
             JObject objetoRetorno = new JObject();
-
             try
             {
-
-                Db.Usuarios.Add(usuario);
-                Db.SaveChanges();
+                var repository = new UsuarioRepository(Db);
+                if (repository.JaExiste(usuario.Email))
+                {
+                    objetoRetorno.Add("success", false);
+                    objetoRetorno.Add("message", "Este email já está em uso.");
+                    return objetoRetorno;
+                }
+                repository.Salvar(usuario);
                 objetoRetorno.Add("success", true);
                 return objetoRetorno;
-
             }
             catch (Exception)
             {
                 throw;
             }
-
         }
     }
 }
---
+++ C19QuizForKids/Repository/UsuarioRepository.cs
@@ -0,0 +1,35 @@
+﻿using C19QuizForKids.Context;
+using C19QuizForKids.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace C19QuizForKids.Repository
+{
+    public class UsuarioRepository
+    {
+        protected readonly MyContext Db;
+
+        public UsuarioRepository(MyContext db)
+        {
+            Db = db;
+        }
+
+        public bool JaExiste(string email)
+        {
+           return Db.Usuarios
+                .Any(x => x.Email == email);
+        }
+
+
+        public bool Salvar(Usuario usuario)
+        {
+            Db.Usuarios.Add(usuario);
+            Db.SaveChanges();
+            return true;
+        }
+
+
+    }
+}
