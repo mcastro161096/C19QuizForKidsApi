d0e0a93585f123dbf00bc1b514795941df98c007
charlesmuller <charlesmuller@rede.ulbra.br>
2021-11-22 22:43:28 -0300

    Criação de repositorym application e controller do ranking

---
+++ C19QuizForKids/Application/RankingApplication.cs
@@ -0,0 +1,22 @@
+﻿using C19QuizForKids.Context;
+using C19QuizForKids.Entities;
+
+namespace C19QuizForKids.Application
+{
+    public class RankingRepository
+    {
+        protected readonly MyContext _context;
+        public RankingRepository(MyContext context)
+        {
+            _context = context;
+        }
+
+        public bool Salvar(Ranking ranking)
+        {
+            RankingRepository rankingRepository = new RankingRepository(_context);
+            return rankingRepository.Salvar(ranking);
+            
+
+        }
+    }
+}
---
+++ C19QuizForKids/Controllers/RankingController.cs
@@ -0,0 +1,69 @@
+﻿using C19QuizForKids.Application;
+using C19QuizForKids.Context;
+using C19QuizForKids.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace C19QuizForKids.Controllers
+{
+    [Route("api/[controller]")]
+    public class RankingController : Controller
+    {
+        protected readonly MyContext _context;
+        public RankingController(MyContext context)
+        {
+            _context = context;
+        }
+
+
+        // GET: RankingController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: RankingController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: RankingController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: RankingController/Create
+        [HttpPost]
+        
+        public ActionResult Create(Ranking ranking)
+        {
+            try
+            {
+                RankingRepository rankingApplication = new RankingRepository(_context);
+                rankingApplication.Salvar(ranking);
+                return Ok();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RankingController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+       
+        // GET: RankingController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+
+    }
+}
