aadb4888c2eb044121bc91d5d83969ffc51f86e8
dsfgdfg <MATHEUSCASTRO161096@gmail.com>
2021-11-29 02:57:13 -0300

    Update insertSQL.sql

--- C19QuizForKids/ScriptsSQL/insertSQL.sql
+++ C19QuizForKids/ScriptsSQL/insertSQL.sql
@@ -7,3 +7,8 @@ insert into cards
 values('Lavar as mãos é importante?', 
  'Lavar as mãos ajuda a matar vírus e bactérias presentes nas mãos!', 
 'http://localhost:3000/img/pergunta3.png', 1 ) 
+
+insert into cards 
+values('É importante avisar quando sentimos dores?', 
+ 'Avisar os nossos pais é muito importante quando sentimos alguma dor, para que eles possam tomar os devidos cuidados!', 
+'http://localhost:3000/img/pergunta4.png', 1 ) 
