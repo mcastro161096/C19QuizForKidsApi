1abc456e9204c28a3250deda8d662f8bd1587b15
dsfgdfg <MATHEUSCASTRO161096@gmail.com>
2021-11-29 00:12:09 -0300

    Subindo script pra inserir as perguntas na base.

---
+++ C19QuizForKids/ScriptsSQL/insertSQL.sql
@@ -0,0 +1,9 @@
+insert into cards 
+values('O covid-19 é um vírus?', 
+ 'A covid-19 é a doença, que é causada pelo Corona Vírus!', 
+'http://localhost:3000/img/pergunta1.png', 0 ) 
+
+insert into cards 
+values('Lavar as mãos é importante?', 
+ 'Lavar as mãos ajuda a matar vírus e bactérias presentes nas mãos!', 
+'http://localhost:3000/img/pergunta3.png', 1 ) 
